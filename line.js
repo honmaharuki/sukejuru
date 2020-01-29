7ee1f26e66ab52990acdf195f704651d339ef61c
aiaia <aiaia.aomiti@gmail.com>
2020-01-29 09:43:51 +0900

    test

---
+++ line.js
@@ -0,0 +1,52 @@
+//LINEに関わる関数
+
+//LINEにアクセスする為のトークンを代入。 トークン...トークンとは場所を記す為の印のようなもの。
+var channel_access_token = "XhvCDRvIv58DGyuFq0pADwWH8cbiREYtY3uAwjAucjCwDtcRc6mNTymbdEueYIY8lniaJd9Gk6Fx6yRfLANNdlsIOm8sMXe5dxwODtK6WwDcsFuUGbEDv3EmNhOCdg2flvhQY7KrWgpQliop6ykWdwdB04t89/1O/w1cDnyilFU=";
+
+//時間
+var moment = Moment.load(); //Momentライブラリをつけるようにする。
+var headers = {
+  "Content-Type": "application/json; charset=UTF-8",
+  "Authorization": "Bearer " + channel_access_token
+};
+
+//ユーザーからのメッセージに反応して返答する関数。
+function sendLineMessageFromReplyToken(token, replyText) {
+  var url = "https://api.line.me/v2/bot/message/reply"; //応答の為のurl。
+  var postData = {
+    "replyToken": token, //どこに返信するか。
+    "messages": [{
+      "type": "text",
+      "text": replyText //送るテキストの情報。
+    }]
+  };
+  console.log(token);
+  var options = {
+    "method": "POST", //POST で送る。
+    "headers": headers, 
+    "payload": JSON.stringify(postData)
+  };
+  console.log(replyText);
+  console.log(url);
+  console.log(options);
+  return UrlFetchApp.fetch(url, options); //LINEに送信。
+}
+
+
+//指定されたUserldにアプリ側から自発的にメッセージを送る関数。　リマインダー機能には必須。
+function sendLineMessageFromUserId(userId, text) { //誰に何を送るのかを引数として取得。
+  var url = "https://api.line.me/v2/bot/message/push"; //送る為のurl。
+  var postData = {
+    "to": userId, //誰に送るか。
+    "messages": [{
+      "type": "text",
+      "text": text //何を送るか。
+    }]
+  };
+  var options = {
+    "method": "POST",
+    "headers": headers,
+    "payload": JSON.stringify(postData)
+  };
+  return UrlFetchApp.fetch(url, options); //LINEに返信。
+}
---
+++ sheet.js
@@ -0,0 +1,259 @@
+//スプレットシートとのやり取りに使われる関数まとめ。
+// メモ化を行う関数
+function getMainSheet(){
+  if(getMainSheet.memoSheet){return getMainSheet.memoSheet;}
+  getMainSheet.memoSheet = SpreadsheetApp.openById("1Gyw_0oNk-sR5VtjxZVsZlEzgl62aDNpcE-DHqTN9a1U");//IDによってどのスプレッドシートを選択するか指定。
+  return getMainSheet.memoSheet;
+}
+// webhookシートを指定する。
+function getWbhook(){
+  if(getWbhook.memoSheet){return getWbhook.memoSheet;}
+
+  getWbhook.memoSheet = getMainSheet().getSheetByName('webhook');
+  return getWbhook.memoSheet;
+}
+// paramシートを指定する。
+function getParam(){
+  if(getParam.memoSheet){return getParam.memoSheet;}
+  getParam.memoSheet = getMainSheet().getSheetByName('param');
+  
+  return getParam.memoSheet;
+}
+// IDResultシートを指定する。
+function getIDResult(){
+  if(getIDResult.memoSheet){return getIDResult.memoSheet;}
+
+  getIDResult.memoSheet = getMainSheet().getSheetByName('IDResult');
+  return getIDResult.memoSheet;
+}
+///--------------------------------------------------------------------------------------
+// dataの取得
+function getValuesWbhook(){
+
+  dat = getWbhook().getDataRange().getValues(); //二次元配列で値を取得。
+  return dat;
+}
+function getValuesIDResult(){
+  ReDat = getIDResult().getDataRange().getValues(); //二次元配列で値を取得。
+  return ReDat;
+}
+
+//-----------------------------------------------------------------------------------------------------
+///--------------------------------------------------------------------------------------
+// // dataの取得
+// var dat = getWbhook.getDataRange().getValues(); //二次元配列で値を取得。
+// var ReDat = getParam.getDataRange().getValues(); //二次元配列で値を取得。
+
+// //-----------------------------------------------------------------------------------------------------
+
+function searchContent() { //内容があるか確認 最終行のrowを出す
+  var ReDat = getValuesIDResult();
+  //検索する値とcolを指定して、見つけた行の番号を返す。なければfalseを返す。
+    //console.log("R16:"+ReDat.length);
+    return ReDat.length;
+}
+
+function searchDate() { //日付があるか確認 日付の最終行のrowを出す
+  var ReDat = getValuesIDResult();
+
+    //検索する値とcolを指定して、見つけた行の番号を返す。なければfalseを返す。
+    lengthI = 0;
+      for (var i = 0; i < ReDat.length; i++) { //.length 配列の要素の個数を返す。 datに入っている値の分回る。
+        if (ReDat[i][2]) { //渡されたuserIdと一致する値があれば何行目に入っていたのかその値を返す。
+          lengthI++;
+        }
+      }
+      // console.log("R28:"+lengthI);
+      return lengthI;
+}
+
+//-----------------------------------------------------------------------------------------------------
+// レコードの最終行にタスクを登録
+function appendToSheetContent( userId, text ) {
+  var sheet = getWbhook();
+  sheet.appendRow(["",userId,"","",text,updateDateNow(),updateTimeNow()]);//シートwebhookにレコードを追加。
+  return '「' + text + ' 」だね。覚えたよ\nいつ教えてほしい？\n例：「10分後」「11月23日17時00分」など\n「○分後」か、「○月○日○時○分」形式で教えてね。そうしないと正しく時間を登録できないよ！\n「キャンセル」って言ってくれればやめるよ。';
+}
+
+// レコードの最後に追加
+function appendToSheet(text) {
+  var sheet = getWbhook();
+  sheet.appendRow([,text]);//シートwebhookにレコードを追加。
+  
+}
+function appendTodataSheet(text) {
+  var sheet = getWbhook();
+  sheet.appendRow([,text]);//シートDataにレコードを追加。
+  
+}
+function searchRowNum(searchVal, col) { //受け取ったシートのデータを二次元配列に取得。
+//検索する値とcolを指定して、見つけた行の番号を返す。なければfalseを返す。
+var dat = getValuesWbhook();
+  for (var i = 0; i < dat.length; i++) { //.length 配列の要素の個数を返す。datに入っている値の分回る。
+    if (dat[i][col] === searchVal) { //渡されたuserIdと一致する値があれば何行目に入っていたのかその値を返す。
+      return i;
+    }
+  }
+  return false;
+}
+// 日付を入れるべきrowを取得
+function insertSearchUseridRowNum() { //受け取ったシートのデータを二次元配列に取得。
+  var ReDat = getValuesIDResult();
+  // ReSheet = spreadsheet.getSheetByName('IDResult');//登録用のシートを選択。  
+  // console.log("RS61:"+ReSheet);
+  // ReDat = ReSheet.getDataRange().getValues(); //二次元配列で値を取得。
+  // console.log("RS63:"+ReDat);
+  //検索する値とcolを指定して、見つけた行の番号を返す。なければfalseを返す。
+    if(ReDat.length == 1){
+      return false;
+    }else{
+      console.log("RS64:"+ReDat);
+      console.log("RS65:"+ReDat[1][0]);
+      return ReDat[1][0];
+    }
+    
+  }
+function searchRowNumData(searchVal, col) { //受け取ったシートのデータを二次元配列に取得。
+//検索する値とcolを指定して、見つけた行の番号を返す。なければfalseを返す。
+var dat = getValuesWbhook();  
+for (var i = 0; i < dat.length; i++) { //.length 配列の要素の個数を返す。datに入っている値の分回る。
+    if (dat[i][col] === searchVal) { //渡されたuserIdと一致する値があれば何行目に入っていたのかその値を返す。
+      return i;
+    }
+  }
+  return false;
+}
+
+function getFromRowCol(sheetName, row, col) {//row,colで場所指定、値 読み込み。
+  var dat = getValuesWbhook();  
+
+  return dat[row][col]; //行、列の指定された値を返す。
+}
+
+function setFromRowCol(val, row, col) {//row,colで場所指定、値 書き込み。
+  var sheet = getWbhook();
+  sheet.getRange(row, col).setValue(val);
+  updateNowSet(row);
+}
+function setDateTimeRowCol(valDate,valTime, row) {//row,colで場所指定、値 書き込み。
+  var sheet = getWbhook();
+
+  sheet.getRange(row, 3).setValue(valDate); //date
+  sheet.getRange(row, 4).setValue(valTime); //time
+  updateNowSet(row);
+}
+function setFromRowColData(val, row, col) {//row,colで場所指定、値 書き込み。
+  var sheet = getWbhook();
+
+  sheet.getRange(row, col).setValue(val);
+}
+function getUserIdCell(row) {// UserId どのLINEから送られた情報か、端末を識別する為の情報のセルを取得
+  var sheet = getWbhook();
+
+  return sheet.getRange(row + 1, 1);
+}
+function getTodoCell(row) { //LINEから送られたタスクが保存されたセルを取得
+  var sheet = getWbhook();
+  return sheet.getRange(row + 1, 2);
+}
+function getUserIdCelldata(row) {// UserId どのLINEから送られた情報か、端末を識別する為の情報のセルを取得
+  var sheet = getWbhook();
+  return sheet.getRange(row + 1, 1);
+}
+function getTodoCelldata(row) { //LINEから送られたタスクが保存されたセルを取得
+  var sheet = getWbhook();
+  return sheet.getRange(row + 1, 2);
+}
+
+function getDateCell(row) {//日付の情報が保存されたセルを取得
+  var sheet = getWbhook();
+  return sheet.getRange(row + 1, 3);
+}
+function getTriggerCell(row) {//トリガーが保存されたセルを取得
+  var sheet = getWbhook();
+  return sheet.getRange(row + 1, 4);
+}
+// 今の日付を返す。
+function updateDateNow(){
+  
+  return  Moment.moment().format('YYYY-MM-DD');
+}
+// 今の時間を返す。
+function updateTimeNow(){
+  return  Moment.moment().format('H:m:0');
+}
+// // 今の時間を返す。
+// function updateNow(){
+//   return  Moment.moment().format('YYYY-MM-DD!H:m:s');
+// }
+// 送られてきた行に今の時間を書き込み
+function updateNowSet(row){
+  var sheet = getWbhook();
+
+  sheet.getRange(row, 6).setValue(updateDateNow());
+  sheet.getRange(row, 7).setValue(updateTimeNow());
+}
+
+
+/****************       param       *****************/
+function setPsheetFromRowColData(val, row, col) {//row,colで場所指定、値 書き込み。
+  var PSheet = getParam();
+
+  PSheet.getRange(row, col).setValue(val);
+}
+
+
+
+/*
+-----------------------------------------------------------------------------------------------
+*/
+
+
+function appendToReSheet(text) {
+ var ReSheet = getIDResult();
+  ReSheet.getRange(1,1).setValue(text);//シートresultにレコードを追加。
+}
+
+// Psheetに入力
+function ToPSheet(text) {
+  var PSheet = getParam();
+
+  PSheet.getRange(1,1).setValue(text);//シートresultにレコードを追加。
+  console.log("RS222:"+text);
+  var insertionUserDataRow = insertSearchUseridRowNum() + 1;
+  console.log("RS221:"+insertSearchUseridRowNum() + 1);
+  return insertionUserDataRow;
+}
+
+function selectQuery(number){
+  
+  switch(number){
+      case 1:
+          appendToReSheet("=Query(webhook!A:F, \"Where B = '\"&param!A1&\"' Order by C asc\")");
+          // クエリにてWHO情報をもとに一致するデータを抜き出す。
+          break;
+      case 2:
+          // 
+          break;
+      default :
+          break;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
